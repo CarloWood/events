01a21336f4316622c6762e5d52e38ee66f813851
Carlo Wood <carlo@alinoe.com>
2019-12-02 21:11:10 +0100

    rewrite_header: add module name and desc.

--- Events.cxx
+++ Events.cxx
@@ -1,4 +1,6 @@
 /**
+ * events -- C++ Core utilities
+ *
  * @file
  * @brief Implementation of Events.
  *
@@ -9,12 +11,12 @@
  *
  * This file is part of events.
  *
- * events is free software: you can redistribute it and/or modify
+ * Events is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * events is distributed in the hope that it will be useful,
+ * Events is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
